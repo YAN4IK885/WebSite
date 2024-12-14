ace40350d37e6810c41fd31fd68202cea437ee04
ProstoChydo <dianasuha885@gmail.com>
2024-12-14 20:50:18 +0200

    Add all without API

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,27 @@ document.addEventListener('DOMContentLoaded', function() {
         menu.classList.toggle('active');
     });
 } );
+
+function changeTextColor(color) {
+    document.querySelector('.banner_heading').style.color = color;
+}
+
+function changeFontSize(size) {
+    document.querySelector('.banner_heading').style.fontSize = size;
+}
+
+function changeContainerWidth(width) {
+    document.querySelector('.banner').style.width = width + 'px';
+}
+
+function changeContainerHeight(height) {
+    document.querySelector('.banner').style.height = height + 'px';
+}
+
+function changeBackgroundColor(color) {
+    document.body.style.backgroundColor = color;
+}
+
+function applyCustomValue(value) {
+    document.querySelector('.banner_heading').style.fontSize = value + 'px';
+}
